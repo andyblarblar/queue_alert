a00f806ef199c2d519e89483722384622833ecbc
Andy Ealovega <andrew@ealovega.dev>
2021-06-06 23:55:31 -0400

    init

---
+++ queue_alert_server/src/main.rs
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2021. Andrew Ealovega
+ */
+
+use actix_web::*;
+
+#[actix_web::main]
+async fn main() {
+//TODO add endpoints for receaving the JSON subscription_info from client (can just deserialize). Then handle VAPID and test with demo frontend. Frontend can be hosted from static files using this same server. Once that is working, make the quetimes scraper and then finish the frontend
+}
