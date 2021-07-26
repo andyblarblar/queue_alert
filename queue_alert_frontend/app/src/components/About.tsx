e7f31799f674036315ae742f01dc348edd2cfac8
Andy Ealovega <andrew@ealovega.dev>
2021-07-26 02:34:54 -0400

    edit About page.

--- queue_alert_frontend/app/src/components/About.tsx
+++ queue_alert_frontend/app/src/components/About.tsx
@@ -16,14 +16,16 @@ function About() {
                     Queue Alert is a Progressive Web App (PWA) that will send notifications whenever an amusement ride reaches
                     a certain wait-time threshold. This app was inspired by a particularly windy trip to Cedar Point,
                     where I spent all day checking <a href="https://queue-times.com">Queue times</a> on my phone
-                    while waiting for SteVe to open.
+                    while waiting for SteVe to open. Queue Alert aims to be a mobile first, streamlined, and ergonomic 
+                    solution to automate checking wait times such that you can spend less time checking your phone, and more time 
+                    enjoying your visit. 
                 </Faq>
             </div>
 
             <div id="FAQ">FAQ</div>
 
-            <Faq title="I'm on IOS and can't save!">
-                Unfortunately, Apple does not support web push notifications on IOS or safari yet.
+            <Faq title="I'm on iOS and can't save!">
+                Unfortunately, Apple does not support web push notifications on iOS or Safari yet.
                 This means that Queue Alert is unable to operate. Because Apple is so protective of its app store,
                 I don't see this going away any time soon. Should this block ever be dropped however, Queue Alert
                 will work immediately.
@@ -46,15 +48,15 @@ function About() {
             </Faq>
 
             <Faq title="Is there any way to submit an issue?">
-                Sure, if you're technical enough. Queue Alerts is FOSS, hosted on <a href="https://github.com/andyblarblar/queue_alert">github. </a>
-                If you want, you can leave an issue on the repo and I'll see if I can work on it. Please dont submit anything on this FAQ,
+                Sure, if you're technical enough. Queue Alerts is FOSS, hosted on <a href="https://github.com/andyblarblar/queue_alert">github.</a> If 
+                you want, you can leave an issue on the repo and I'll see if I can work on it. Please dont submit anything on this FAQ,
                 and provide your browser version for any issues. Thanks!
             </Faq>
 
             <Faq title="Can I configure multiple parks at once?">
                 Unfortunately, Queue Alert was not designed for multiple parks at once. While this Could
                 (and may) be added at a later time, I began to hit transfer size constraints when multiple parks
-                where used. Should the push notification be compressed, this would be possible, yet the server
+                were used. Should the push notification be compressed, this would be possible, yet the server
                 refuses to send when this is applied {'>'}:[
             </Faq>
 
