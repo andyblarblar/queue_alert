ba69156800dd776c7d358194f950e7e64c301f28
Andy Ealovega <andrew@ealovega.dev>
2021-08-22 00:05:06 -0400

    Add message compression. This greatly decreases the payload size from a few kb to <= 500B. This allows for all parks to send, fixing #7. Should also reduce data usage.
    Closes #8.

--- queue_alert_server/src/main.rs
+++ queue_alert_server/src/main.rs
@@ -2,22 +2,25 @@
  * Copyright (c) 2021. Andrew Ealovega
  */
 
-mod models;
-mod routes;
+use std::io::{Read, Write};
+use std::sync::Arc;
 
 use actix_files::Files;
 use actix_web::middleware::Logger;
 use actix_web::web::Data;
 use actix_web::*;
+use flate2::write::GzEncoder;
+use flate2::Compression;
 use iis::get_port;
 use queue_times::client::QueueTimesClient;
 use simplelog::{ConfigBuilder, LevelFilter};
-use std::io::Read;
-use std::sync::Arc;
 use web_push::{
     ContentEncoding, PartialVapidSignatureBuilder, VapidSignatureBuilder, WebPushClient,
     WebPushError, WebPushMessageBuilder,
 };
+
+mod models;
+mod routes;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -121,6 +124,16 @@ async fn main() -> std::io::Result<()> {
 
                 let content = serde_json::to_string(&rides.unwrap()).unwrap();
 
+                log::info!("JSON used: {}", content);
+
+                //Compress JSON with gzip
+                let mut encoder = GzEncoder::new(Vec::new(), Compression::best());
+                encoder.write_all(content.as_bytes()).unwrap();
+                let content = encoder.finish().unwrap();
+                //URLbase64 encode so the browser accepts the bytes //TODO change to reuse string to avoid allocs. It doesnt seem to be base64 encodig right.
+                let content = base64::encode(&content);
+
+                log::info!("Encoded used: {}", content);
                 log::debug!("Content size: {} bytes", content.len());
 
                 //*Must* set vapid signature, else error
@@ -191,4 +204,43 @@ fn load_private_key() -> std::io::Result<(
     log::info!("Using pub key: {}", final_pub);
 
     Ok((models::PrivateKey(str), models::PublicKey(final_pub), sig))
+}
+
+#[cfg(test)]
+mod test {
+    use std::io::{Write, Read};
+
+    use flate2::write::GzEncoder;
+    use flate2::Compression;
+    use serde_json::Value;
+    use flate2::read::GzDecoder;
+
+    fn get_content() -> Value {
+        serde_json::json!([{"name":"Blue Streak","status":"Closed"},{"name":"Cedar Creek Mine Ride","status":"Closed"},{"name":"Corkscrew","status":"Closed"},{"name":"GateKeeper","status":"Closed"},{"name":"Gemini","status":"Closed"},{"name":"Iron Dragon","status":"Closed"},{"name":"Magnum XL-200","status":"Closed"},{"name":"Maverick","status":"Closed"},{"name":"Millennium Force","status":"Closed"},{"name":"Pipe Scream","status":"Closed"},{"name":"Raptor","status":"Closed"},{"name":"Rougarou","status":"Closed"},{"name":"Steel Vengeance","status":"Closed"},{"name":"Top Thrill Dragster","status":"Closed"},{"name":"Valravn","status":"Closed"},{"name":"Wicked Twister","status":"Closed"},{"name":"Wilderness Run","status":"Closed"},{"name":"Woodstock Express","status":"Closed"},{"name":"4x4's","status":"Closed"},{"name":"Antique Cars","status":"Closed"},{"name":"Cadillac Cars","status":"Closed"},{"name":"Charlie Brown's Wind-Up","status":"Closed"},{"name":"Flying Ace Balloon Race","status":"Closed"},{"name":"Giant Wheel","status":"Closed"},{"name":"Midway Carousel","status":"Closed"},{"name":"Peanuts 500","status":"Closed"},{"name":"Peanuts Road Rally","status":"Closed"},{"name":"Snoopy's Deep Sea Divers","status":"Closed"},{"name":"Snoopy's Express Railroad","status":"Closed"},{"name":"Tilt-A-Whirl","status":"Closed"},{"name":"Woodstock's Whirlybirds","status":"Closed"},{"name":"Dune Buggies","status":"Closed"},{"name":"Helicopters","status":"Closed"},{"name":"Joe Cool's Dodgem School","status":"Closed"},{"name":"Motorcycles","status":"Closed"},{"name":"Police Cars","status":"Closed"},{"name":"Rock, Spin, and Turn","status":"Closed"},{"name":"Roto Whip","status":"Closed"},{"name":"Sky Fighters","status":"Closed"},{"name":"Snoopy's Space Race","status":"Closed"},{"name":"Space Age","status":"Closed"},{"name":"Cedar Downs Racing Derby","status":"Closed"},{"name":"Dodgem","status":"Closed"},{"name":"Matterhorn","status":"Closed"},{"name":"MaXair","status":"Closed"},{"name":"Monster","status":"Closed"},{"name":"Power Tower","status":"Closed"},{"name":"Scrambler","status":"Closed"},{"name":"Skyhawk","status":"Closed"},{"name":"SlingShot","status":"Closed"},{"name":"Super Himalaya","status":"Closed"},{"name":"Thunder Canyon","status":"Closed"},{"name":"Tiki Twirl","status":"Closed"},{"name":"Troika","status":"Closed"},{"name":"Fisherman's Fury","status":"Open"}])
+    }
+
+    #[test]
+    fn test_compress_base64() {
+        let content = get_content().to_string();
+
+        //Compress JSON with gzip
+        let mut encoder = GzEncoder::new(Vec::new(), Compression::best());
+        encoder.write_all(content.as_bytes()).unwrap();
+        let content = encoder.finish().unwrap();
+        //URLbase64 encode so the browser accepts the bytes
+        let content = base64::encode(&content);
+
+        println!("encoded: {}", content);
+
+        assert!(content.as_bytes().len() < 4000);
+        let un_based = base64::decode(&content).unwrap();
+
+        let mut decoder = GzDecoder::new(&*un_based);
+        assert!(decoder.header().is_some());
+
+        let mut new_content = String::new();
+        decoder.read_to_string(&mut new_content).unwrap();
+
+        assert_eq!(new_content, get_content().to_string())
+    }
 }
